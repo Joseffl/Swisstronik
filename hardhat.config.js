6be7f162f5bfbdb21aada714bae1d7852b9b61aa
Joseffl <78298373+Joseffl@users.noreply.github.com>
2024-08-17 14:02:44 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: ["0xe4c88b69b9169556d0c2af1a6f76cb86e628d708d9cb99ea09fc32b26b8852a5"], //Your private key starting with "0x"
+      accounts: [""], //Your private key starting with "0x"
     },
   },
 };
